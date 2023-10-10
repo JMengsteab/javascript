f906c2889643a3ee4261e13b4746b91bfa989976
JMengsteab <302247045@student.rocmondriaan.nl>
2023-10-10 12:51:59 +0200

    Update main.js

--- js functions/main.js
+++ js functions/main.js
@@ -50,5 +50,194 @@ showArrayOfBrands()
 
 //opdracht 7
 
-let computer = 7
-let user = 5
+let computer = 7;
+
+let user = 5;
+
+let bid;
+
+ 
+
+function printVariables() {
+
+    console.log("Computer: " + computer);
+
+    console.log("User: " + user);
+
+    console.log("Bid: " + bid);
+
+}
+
+//opdracht 8
+
+function showMessage(message) {
+    console.log(message);
+}
+
+showMessage("Hoi, hoe gaat het allemaal?")
+
+//opdracht 9
+
+function showNumbers(a, b) {
+    console.log("a", a);
+    console.log("b", b);
+}
+showNumbers(12, 24)
+
+//opdracht 10
+function showSum(a, b) {
+    console.log("sum is", a + b);
+}
+showSum(4, 5)
+
+//opdr 11
+
+function showSum(num1, num2) {
+
+    console.log("Som: " + (num1 + num2));
+
+}
+
+showSum(5, 3);
+
+ 
+
+//opdr 12
+
+function concatenateStrings(str1, str2) {
+
+    console.log(str1 + " " + str2);
+
+}
+
+concatenateStrings("Hello", "World");
+
+ 
+
+//opdr 13
+
+function showArray(arr) {
+
+    arr.forEach(item => {
+
+        console.log(item);
+
+    });
+
+}
+
+ 
+
+const myArray = [1, 2, 3, 4, 5];
+
+showArray(myArray);
+
+ 
+//opdr 15
+
+function getSum(num1, num2) {
+
+    return num1 + num2;
+
+}
+
+
+const result = getSum(10, 5);
+
+console.log("De som is: " + result);
+
+ 
+
+//opdr 16
+
+function getProduct(num1, num2) {
+
+    return num1 * num2;
+
+}
+
+ 
+
+result = getProduct(6, 7);
+
+console.log("Het product is: " + result);
+
+ 
+
+//opdr 17
+
+function getDivision(num1, num2) {
+
+    if (num2 === 0) {
+
+        return 0;
+
+    } else {
+
+        return num1 / num2;
+
+    }
+
+}
+
+ 
+
+ result = getDivision(8, 2);
+
+console.log("Deel-som: " + result);
+
+ 
+
+//opdr18
+
+function getConcatenatedStrings(str1, str2) {
+
+    if (str1 === null || str2 === null) {
+
+        return null;
+
+    } else {
+
+        return str1 + " " + str2;
+
+    }
+
+}
+
+ 
+
+result = getConcatenatedStrings("Hello", "World");
+
+console.log(result);
+
+ 
+
+//opdr 19
+
+function getSumOfArray(arr) {
+
+    let sum = 0;
+
+ 
+
+    for (let i = 0; i < arr.length; i++) {
+
+        sum += arr[i];
+
+    }
+
+ 
+
+    return sum;
+
+}
+
+ 
+
+numbers = [1, 3, 5, 7, 11];
+
+result = getSumOfArray(numbers);
+
+console.log("De som van de array is: " + result);
+
+ 
