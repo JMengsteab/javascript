df993b0c9e5c8ecb93febf488e2b81e1e97bda49
JMengsteab <302247045@student.rocmondriaan.nl>
2023-11-09 12:54:27 +0100

    herhalingsopdrachten

---
+++ js herhalingsopdrachten/main.js
@@ -0,0 +1,9 @@
+console.log("Het werkt");
+
+//opdracht 1
+let salary = 2000;
+let raise = salary * .05;
+console.log("Verhoging is €", raise);
+let newSalary = salary + raise;
+
+document.body.textContent = newSalary
